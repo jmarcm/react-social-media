d3c8d6cdd96bf7151a6a590c542d7011719d4f77
Jean-Marc <mjean_marc@hotmail.com>
2020-12-09 08:06:42 +0100

    Display username in title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
-import Login from "./components/Login";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import CreatePost from "./components/CreatePost";
 
 function App() {
     const [user, setUser] = React.useState("jm");
 
+    React.useEffect(() => {
+        document.title = user ? `${user}'s feed` : "Please login";
+    }, [user]);
+
     if (!user) {
         return <Login setUser={setUser} />;
     }
+
     return (
         <>
             <Header user={user} setUser={setUser} />
+            <CreatePost />
         </>
     );
 }
