8f0a919fd183f301dfcb2739fcc9e95112d658f6
Jean-Marc <mjean_marc@hotmail.com>
2020-12-09 08:06:58 +0100

    Start create post logic

---
+++ src/components/CreatePost.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CreatePost() {
+    return (
+        <div>
+            <h1>Create Post</h1>
+            <form>
+                <input type="text" placeholder="Add Post Content" />
+                <input type="file" />
+                <button type="submit">Submit Post</button>
+            </form>
+        </div>
+    );
+}
+
+export default CreatePost;
