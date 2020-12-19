31b23e8afef511f86110ac1a27884a123b90a259
Jean-Marc <mjean_marc@hotmail.com>
2020-12-19 12:46:31 +0100

    Clean up code

--- src/App.js
+++ src/App.js
@@ -7,21 +7,15 @@ import PostList from "./components/PostList";
 function App() {
     const [user, setUser] = React.useState("jm");
     const [posts, setPosts] = React.useState([]);
-    const [count, setCount] = React.useState(0);
 
     React.useEffect(() => {
         document.title = user ? `${user}'s feed` : "Please login";
     }, [user]);
 
-    const functionsCount = new Set();
-
     const handleAddPost = React.useCallback(
         (newPost) => setPosts([newPost, ...posts]),
         [posts]
     );
-
-    functionsCount.add(handleAddPost);
-    console.log(functionsCount);
 
     if (!user) {
         return <Login setUser={setUser} />;
@@ -32,10 +26,6 @@ function App() {
             <Header user={user} setUser={setUser} />
             <CreatePost user={user} handleAddPost={handleAddPost} />
             <PostList posts={posts} />
-
-            <button onClick={() => setCount((prev) => prev + 1)}>
-                {count} +
-            </button>
         </>
     );
 }
