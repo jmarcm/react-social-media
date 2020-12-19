9199a6fd413402e1f1832beec1081bee4284205d
Jean-Marc <mjean_marc@hotmail.com>
2020-12-19 10:47:48 +0100

    Fix bug when post has no image

--- src/App.js
+++ src/App.js
@@ -23,15 +23,17 @@ function App() {
             {posts.map((post, index) => {
                 return (
                     <React.Fragment key={index}>
-                        <img
-                            src={URL.createObjectURL(post.image)}
-                            alt=""
-                            style={{
-                                width: 200,
-                                height: 100,
-                                objectFit: "cover",
-                            }}
-                        />
+                        {post.image && (
+                            <img
+                                src={URL.createObjectURL(post.image)}
+                                alt=""
+                                style={{
+                                    width: 200,
+                                    height: 100,
+                                    objectFit: "cover",
+                                }}
+                            />
+                        )}
                         <p>{post.content}</p>
                         <p>{post.user}</p>
                     </React.Fragment>
