07a247208b6c4e35cfde39cb008f20f89ac573a6
Jean-Marc <mjean_marc@hotmail.com>
2020-12-19 12:37:35 +0100

    Every type the componant reender a new function is added?

--- src/App.js
+++ src/App.js
@@ -7,14 +7,23 @@ import PostList from "./components/PostList";
 function App() {
     const [user, setUser] = React.useState("jm");
     const [posts, setPosts] = React.useState([]);
+    const [count, setCount] = React.useState(0);
 
     React.useEffect(() => {
         document.title = user ? `${user}'s feed` : "Please login";
     }, [user]);
 
+    const functionsCount = new Set();
+
     function handleAddPost(newPost) {
         setPosts([newPost, ...posts]);
     }
+
+    /**
+     * Every type the componant reender a new function is added ?
+     */
+    functionsCount.add(handleAddPost);
+    console.log(functionsCount);
 
     if (!user) {
         return <Login setUser={setUser} />;
@@ -25,6 +34,10 @@ function App() {
             <Header user={user} setUser={setUser} />
             <CreatePost user={user} handleAddPost={handleAddPost} />
             <PostList posts={posts} />
+
+            <button onClick={() => setCount((prev) => prev + 1)}>
+                {count} +
+            </button>
         </>
     );
 }
